04217e0e7551a0b93a41f393a6ea367a49cc3e09
Rakibul Sayed <sarahasa_@lib1202-09.lib.jyu.fi>
2017-01-12 16:05:36 +0200

    DIfferent validation to different controls have been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { MultiselectDropdown, IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'angular-2-dropdown-multiselect/src/multiselect-dropdown';
 
-import { MethodService, Method, DisciplineService, Discipline } from './shared/index'
+import { MethodService, Method, DisciplineService, Discipline } from './shared/index';
+
 
 
 @Component({
@@ -14,13 +15,30 @@ export class AppComponent {
 
 	constructor(private methodListService: MethodService, 
 				private disciplineService: DisciplineService) {}
-
-	methods: Method[] = this.methodListService.getMethods();
-	disciplines: Discipline[] = this.disciplineService.getDisciplines();
+	
+	private selectedOptions_method: number[];
+	private selectedOptions_discip: number[];
+	private methods: IMultiSelectOption[] = this.methodListService.getMethods();
+	private disciplines: IMultiSelectOption[] = this.disciplineService.getDisciplines();
 
 	getMethodList(): void {
 		//this.methodList = this.methodListService.getMethods();
 		//this.methodListService.getMethods().then(methodList => this.methodList = methodList);
+	}
+
+	display: any ;
+
+	goSearch({value, valid}: {value: any, valid: boolean}){
+		//this.display= f.searchText.value; 
+		console.log(value.searchText);
+		console.log(value.year_from);
+		console.log(value.selected_method.name);
+		console.log(value, valid);
+	}
+
+	onChange($event){
+		console.log(event);
+		this.display = event ;
 	}
 
 	//private selectedOptions: number[];
@@ -29,15 +47,15 @@ export class AppComponent {
 		{ id: 3, name: 'Denmark' }, { id: 4, name: 'Finland' },
 	];
 
-	private texts_method: IMultiSelectTexts = {
+	private txt_method: IMultiSelectTexts = {
 		defaultTitle: 'Select Method(s)'
 	};
 
-	private texts_discipline: IMultiSelectTexts = {
+	private txt_discipline: IMultiSelectTexts = {
 		defaultTitle: 'Select Discipline(s)'
 	};
 
-	private selectSettings_withSearch: IMultiSelectSettings = {
+	private settings_Search: IMultiSelectSettings = {
 		enableSearch: true,
 		dynamicTitleMaxItems: 0,
 	};
